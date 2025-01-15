3b6086f3e055d18e56cbe9bbcef367f331080de0
Arian Stolwijk <arian@giftomatic.io>
2025-01-15 17:32:46 +0100

    Export the CopyAndGo class as ES module

--- copy-and-go.js
+++ copy-and-go.js
@@ -1,92 +1,87 @@
-class CopyNgoHandler {
+export class CopyAndGo {
   constructor(config = {}) {
-    this.config = this.initializeConfig(config);
-    this.form = document.getElementById(this.config.formId);
-    this.copyButton = document.getElementById(this.config.copyButtonId);
+    this.config = {
+      notificationId: config.notificationId || null,
+      callback: config.callback || null,
+      giftcodeId: config.giftcodeId || "giftcode",
+      copyButtonId: config.copyButtonId || "copy-go",
+      formId: config.formId || "copy-go-form",
+      timeout: getValidatedTimeout(config.timeout),
+      safariFormTarget: config.safariFormTarget || "_self",
+    };
 
-    if (!this.form || !this.copyButton) {
+    this._form = document.getElementById(this.config.formId);
+    this._copyButton = document.getElementById(this.config.copyButtonId);
+
+    if (!this._form || !this._copyButton) {
       console.warn("Required elements are missing. Initialization aborted.");
       return;
     }
 
-    this.attachEventListeners();
+    this.attach();
   }
 
-  initializeConfig(config) {
-    return {
-      notificationId: config.notificationId || null,
-      giftcodeId: config.giftcodeId || "giftcode",
-      copyButtonId: config.copyButtonId || "copy-go",
-      formId: config.formId || "copy-go-form",
-      timeout: this.getValidatedTimeout(config.timeout),
-      safariFormTarget: config.safariFormTarget || "_self",
-    };
-  }
-
-  getValidatedTimeout(timeout) {
-    const parsedTimeout = parseInt(timeout, 10);
-    return isNaN(parsedTimeout) || parsedTimeout < 0 ? 3000 : parsedTimeout;
-  }
-
-  isSafari() {
-    const ua = navigator.userAgent.toLowerCase();
-    return (
-      ua.includes("safari") &&
-      !ua.includes("chrome") &&
-      !ua.includes("chromium")
-    );
-  }
-
-  attachEventListeners() {
-    this.copyButton.addEventListener("click", () => {
-      this.copyToClipboard();
-      if (this.config.notificationId) {
-        this.showNotification();
-      }
-    });
-
-    if (this.shouldAttachFormListener()) {
-      this.form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        this.handleFormSubmissionWithDelay();
-      });
-    }
-  }
-
-  shouldAttachFormListener() {
-    return (
+  attach() {
+    this._copyButton.addEventListener("click", this._clickListener);
+    const shouldAttachFormListener =
       this.config.timeout > 0 &&
-      (!this.isSafari() || this.config.safariFormTarget !== "_self")
-    );
+      (!isSafari() || this.config.safariFormTarget !== "_self");
+    if (shouldAttachFormListener) {
+      this._form.addEventListener("submit", this._submitListener);
+    }
   }
 
-  handleFormSubmissionWithDelay() {
-    if (this.isSafari() && this.config.safariFormTarget === "_self") {
-      this.form.target = "_self";
+  detach() {
+    this._copyButton.removeEventListener("click", this._clickListener);
+    this._form.removeEventListener("submit", this._submitListener);
+  }
+
+  _clickListener = () => {
+    copyToClipboard(this.config.giftcodeId);
+    if (this.config.notificationId) {
+      const notification = document.getElementById(this.config.notificationId);
+      if (notification) {
+        notification.classList.add("active");
+      }
+    } else if (this.config.callback) {
+      this.config.callback();
+    }
+  };
+
+  _submitListener = (event) => {
+    event.preventDefault();
+
+    if (isSafari() && this.config.safariFormTarget === "_self") {
+      this._form.target = "_self";
     }
 
-    this.copyButton.disabled = true;
+    this._copyButton.disabled = true;
     setTimeout(() => {
-      this.copyButton.disabled = false;
-      this.form.submit();
+      this._copyButton.disabled = false;
+      this._form.submit();
     }, this.config.timeout);
-  }
+  };
+}
 
-  showNotification() {
-    const notification = document.getElementById(this.config.notificationId);
-    if (notification) {
-      notification.classList.add("active");
-    }
-  }
+function isSafari() {
+  const ua = navigator.userAgent.toLowerCase();
+  return (
+    ua.includes("safari") && !ua.includes("chrome") && !ua.includes("chromium")
+  );
+}
 
-  copyToClipboard() {
-    const giftcode = document.getElementById(this.config.giftcodeId);
-    if (giftcode) {
-      giftcode.select();
-      giftcode.setSelectionRange(0, 99999);
-      navigator.clipboard.writeText(giftcode.value).catch((err) => {
-        console.error("Failed to copy text: ", err);
-      });
-    }
+function getValidatedTimeout(timeout) {
+  const parsedTimeout = parseInt(timeout, 10);
+  return isNaN(parsedTimeout) || parsedTimeout < 0 ? 3000 : parsedTimeout;
+}
+
+function copyToClipboard(elementId) {
+  const giftcode = document.getElementById(elementId);
+  if (giftcode) {
+    giftcode.select();
+    giftcode.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(giftcode.value).catch((err) => {
+      console.error("Failed to copy text: ", err);
+    });
   }
 }
